3232ba8afe55aabc24a1fd953cd40f9a0119754c
ma <guerinmeschino@gmail.com>
2021-04-22 23:43:45 +0200

    Update eta' selezionando x cognome

--- src/main/java/it/room/javaanddb/CreaDemo.java
+++ src/main/java/it/room/javaanddb/CreaDemo.java
@@ -3,9 +3,13 @@ package it.room.javaanddb;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CreaDemo {
+
     // JDBC driver name and database URL 
+    public static final String NOME_TABELLA = "TEST";
 
     public void creaTabella() throws SQLException {
 
@@ -18,8 +22,8 @@ public class CreaDemo {
         }
         //STEP 2: Eseguire un' interrogazione(query)
         System.out.println("Crea una tabella nel database...");
-        String sql = "CREATE TABLE   REGISTRATION "
-                + "(id INTEGER not NULL, "
+        String sql = "CREATE TABLE " + NOME_TABELLA
+                + " (id INTEGER not NULL, "
                 + " nome VARCHAR(255), "
                 + " cognome VARCHAR(255), "
                 + " eta INTEGER, "
@@ -29,7 +33,7 @@ public class CreaDemo {
         DbConnect.closeConnection();
     }
 
-    public void cancellaTabella(String nomeTabella) throws SQLException{
+    public void cancellaTabella(String nomeTabella) throws SQLException {
 
         //Passo 1: Aprire connessione
         System.out.println("Connessione al database...");
@@ -38,47 +42,70 @@ public class CreaDemo {
             System.out.println("Connessione rifiutata, esco...");
             System.exit(-1);
         }
-        stmt.execute("drop table "+nomeTabella);
+        stmt.execute("drop table if exists " + nomeTabella);
         stmt.closeOnCompletion();
         DbConnect.closeConnection();
     }
-    
-    public void inserisciRecordsPerTest() throws SQLException{
+
+    public void inserisciRecordsPerTest() throws SQLException {
         Statement stmt = DbConnect.getConnectionStatement();
         if (stmt == null) {
             System.out.println("Connessione rifiutata, esco...");
             System.exit(-1);
         }
-        stmt.execute("insert into REGISTRATION values(1, 'Mario','Rossi',32)");
-        stmt.execute("insert into REGISTRATION values(2, 'Luigi','Bianchi',45)");
-        stmt.execute("insert into REGISTRATION values(3, 'Giulia','Azzurra',18)");
-        stmt.execute("insert into REGISTRATION values(4, 'Serena','Verdi',29)");
+        stmt.execute("insert into " + NOME_TABELLA + " values(1, 'Mario','Rossi',32)");
+        stmt.execute("insert into " + NOME_TABELLA + " values(2, 'Luigi','Bianchi',45)");
+        stmt.execute("insert into " + NOME_TABELLA + " values(3, 'Giulia','Azzurra',18)");
+        stmt.execute("insert into " + NOME_TABELLA + " values(4, 'Serena','Verdi',29)");
         stmt.closeOnCompletion();
         DbConnect.closeConnection();
     }
-    
-    
-    public void leggiRecords() throws SQLException{
+
+    public boolean updateRecord(String cognome, int nuovaEta) {
         Statement stmt = DbConnect.getConnectionStatement();
         if (stmt == null) {
             System.out.println("Connessione rifiutata, esco...");
             System.exit(-1);
         }
-        ResultSet rs = stmt.executeQuery("select * from REGISTRATION");
- 
+        try {
+            stmt.executeUpdate("UPDATE " + NOME_TABELLA + " SET eta="+nuovaEta+" WHERE cognome='" + cognome+"'");
+        } catch (SQLException ex) {
+            Logger.getLogger(CreaDemo.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        return true;
+    }
+
+    public void leggiRecords() throws SQLException {
+        Statement stmt = DbConnect.getConnectionStatement();
+        if (stmt == null) {
+            System.out.println("Connessione rifiutata, esco...");
+            System.exit(-1);
+        }
+        ResultSet rs = stmt.executeQuery("select * from " + NOME_TABELLA);
+
         while (rs.next()) {
-            System.out.println("id " + rs.getInt("id") + 
-                    " nome " + rs.getString("nome") +
-                    " cognome " + rs.getString("cognome") +
-                    " eta " + rs.getString("eta") 
-                    );
+            System.out.println("id " + rs.getInt("id")
+                    + " nome " + rs.getString("nome")
+                    + " cognome " + rs.getString("cognome")
+                    + " eta " + rs.getString("eta")
+            );
         }
+        System.out.println("-------------------------------------------------");
     }
-    
+
     public static void main(String[] args) throws SQLException {
         CreaDemo creaDemo = new CreaDemo();
-        creaDemo.cancellaTabella("REGISTRATION");
+        creaDemo.cancellaTabella(NOME_TABELLA);
         creaDemo.creaTabella();
         creaDemo.inserisciRecordsPerTest();
+        creaDemo.leggiRecords();
+        boolean ret = creaDemo.updateRecord("Azzurra", 28);
+        if(!ret){
+            System.out.println("Update record non eseguito correttamente");
+        } else {
+            System.out.println("Update record eseguito");            
+        }
+        creaDemo.leggiRecords();
     }
 }
